f3da6ed20b04090c2df3d52ae5242f15712c4121
jacob-zachariah <jacu.ker14@gmail.com>
2016-08-03 16:05:17 +0100

    All files commit

---
+++ src/test/java/com/maven/test/pack/SampleTestNG.java
@@ -0,0 +1,18 @@
+package com.maven.test.pack;
+
+import org.testng.annotations.Test;
+
+public class SampleTestNG {
+
+	public SampleTestNG()
+	{
+		
+	}
+	
+@Test
+public void sampleTest()
+{
+	System.out.println("sAMPLE mAVEN PROJECT");
+}
+	
+}
